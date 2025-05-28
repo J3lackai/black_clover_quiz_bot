b177f15ae5049776c0b91e6dbab2ab2d32bc6380
J3lackai <am19012004@gmail.com>
2025-05-29 00:45:22 +0700

    gitignore неправильно был написан

---
+++ main.py
@@ -0,0 +1,8 @@
+import pydotenv
+import aiogram
+import configparser
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+import os
