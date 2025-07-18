ac4b5ff6997fb5c1c454c099e8b483197490f1df
J3lackai <am19012004@gmail.com>
2025-07-18 18:30:26 +0700

    Оптимизация lexicon en/ru, keyboards для легкого масштабирования

--- keyboards/keyboard_utils.py
+++ keyboards/keyboard_utils.py
@@ -1,6 +1,6 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from loguru import logger
+from random import shuffle
 
 
 def get_answers_keyboard(num_of_q: int, lexicon: dict) -> ReplyKeyboardMarkup:
@@ -11,12 +11,19 @@ def get_answers_keyboard(num_of_q: int, lexicon: dict) -> ReplyKeyboardMarkup:
     :return: ReplyKeyboardMarkup
     """
     kb_builder = ReplyKeyboardBuilder()
-
-    answers_key = f"a{num_of_q}"
-    if answers_key not in lexicon:
-        logger.error(f"Отсутствующие ответы на вопрос {num_of_q} в LEXICON_{lexicon}")
-        raise KeyError()
-    buttons = [KeyboardButton(text=option) for option in lexicon[answers_key]]
+    right_ch = lexicon[f"r{num_of_q}"]
+    set_chs = lexicon["characters"]
+    set_chs.discard(right_ch)
+    buttons = list()
+    buttons.append(KeyboardButton(text=right_ch))  # Кнопка с правильным персонажем
+    for ch in set_chs:
+        if len(buttons) == 10:
+            break
+        # Кнопки с рандомными персонажами
+        buttons.append(KeyboardButton(text=ch))
+    shuffle(
+        buttons
+    )  # Перемешали чтобы правильный персонаж был рандомной кнопкой, а не первой
     kb_builder.row(*buttons, width=4)
     return kb_builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
 
